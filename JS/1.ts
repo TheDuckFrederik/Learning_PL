cc1df5e8c245cdaf8c3d1529366f6b5ef5e64310
FrederikTheDuck <pujolunai@proton.me>
2024-10-15 08:44:16 +0200

    0002

---
+++ JS/1.ts
@@ -0,0 +1,18 @@
+//
+var first_name = window.prompt("What is your first name?");
+var age = prompt("What is your age?");
+//
+var choice = confirm("Do you want to see your name?");
+if (choice = true) {
+    alert(first_name);
+} else {
+    alert("Sure");
+}
+//
+var choice2 = confirm("Do you want to see your age?");
+if (choice = true) {
+    alert(age);
+} else {
+    alert("Ok then...");
+}
+//
--- Nim/Nim_101/USD_to_EUR.nim
+++ Nim/Nim_101/USD_to_EUR.nim
@@ -1,40 +1,40 @@
 #
-import strutils
+import strutils;
 #
-var eur_to_usd = 1.08
-var usd_to_eur = 0.92
+var eur_to_usd = 1.08;
+var usd_to_eur = 0.92;
 #
-var mult = 0.00
-var result = 0.00
+var mult = 0.00;
+var result = 0.00;
 #
-var Input_coin: string = ""
-var Output_coin: string = ""
+var Input_coin: string = "";
+var Output_coin: string = "";
 #
 while true:
-    echo "Welcome\n1. EUR to USD\n2. USD to EUR"
-    var choiceStr = readLine(stdin)
-    var choice = choiceStr.parseInt()
+    echo ("Welcome\n1. EUR to USD\n2. USD to EUR");
+    var choiceStr = readLine(stdin);
+    var choice = choiceStr.parseInt();
     #
     if choice == 1:
-        Input_coin = "EUR"
-        Output_coin = "USD"
-        mult = eur_to_usd
-        break
+        Input_coin = "EUR";
+        Output_coin = "USD";
+        mult = eur_to_usd;
+        break;
     #
     elif choice == 2:
-        Input_coin = "USD"
-        Output_coin = "EUR"
-        mult = usd_to_eur
-        break
+        Input_coin = "USD";
+        Output_coin = "EUR";
+        mult = usd_to_eur;
+        break;
     #
     else:
-        echo "Syntax ERR\nPlease try again\n"
+        echo "Syntax ERR\nPlease try again\n";
 #
-echo "\nInsert amount in ", Input_coin, ":"
-var amountStr = readLine(stdin)
-var amountInt = amountStr.parseInt()
-var amount = amountInt.float
+echo "\nInsert amount in ", Input_coin, ":";
+var amountStr = readLine(stdin);
+var amountInt = amountStr.parseInt();
+var amount = amountInt.float;
 #
-result = amount * mult
-echo "\n", result, " ", Output_coin
+result = amount * mult;
+echo ("\n", result, " ", Output_coin);
 #
